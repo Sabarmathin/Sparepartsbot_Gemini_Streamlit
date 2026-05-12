9a0786319dbc85d56e1b1ca710b67414d2a10273
Sabarmathi Natarajan <sabarmathin@gmail.com>
2026-05-12 19:04:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os
 import json
-import google.generativeai as genai
+import google.genai as genai
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 import gspread
